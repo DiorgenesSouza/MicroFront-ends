8488e45d0846187d03f9bf3d99e836ef5bfabaea
DiorgenesSouza <diorgeneslimadesouza833@gmail.com>
2026-01-22 17:54:41 -0300

    exportação de mais componentes de um mesmo Microfront-end

--- produtos/src/app/app.routes.ts
+++ produtos/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { App } from './app'; // Remova o .ts do final, o Angular já entende
+import { Carrinho } from './carrinho/carrinho'; // Ajustado para a pasta que aparece na sua imagem
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'produtos', component: App },
+    { path: 'carrinho', component: Carrinho },
+    { path: '', redirectTo: 'produtos', pathMatch: 'full' }
+];
--- produtos/src/app/app.ts
+++ produtos/src/app/app.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+
+interface Produto {
+  nome: string;
+  valor: number;
+  imagem: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('produtos');
+  produtos: Produto[] = [
+  {
+    nome: 'Camiseta Angular',
+    valor: 79.90,
+    imagem: 'https://images.unsplash.com/photo-1576566588028-4147f3842f27?q=80&w=400&auto=format&fit=crop'
+  },
+  {
+    nome: 'Caneca Programador',
+    valor: 49.90,
+    imagem: 'https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?q=80&w=400&auto=format&fit=crop'
+  },
+  {
+    nome: 'Mouse Gamer RGB',
+    valor: 129.90,
+    imagem: 'https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=400&auto=format&fit=crop'
+  },
+  {
+    nome: 'Teclado Mecânico',
+    valor: 299.00,
+    imagem: 'https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?q=80&w=400&auto=format&fit=crop'
+  },
+  {
+    nome: 'Headset Bluetooth',
+    valor: 199.90,
+    imagem: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=400&auto=format&fit=crop'
+  }
+];
 }
